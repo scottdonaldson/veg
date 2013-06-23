eb07e42d6560bd270c5624742e4e24c6aa8ad78f
Scott Donaldson <scott.p.donaldson@gmail.com>
2013-06-22 20:53:36 -0400

    think it's really, really close

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
-var header = $('header');
+var header = $('header'),
+	breakPoint = 600,
+	delay = 500;
 
 $(window).load(function(){
-	var images = $('.restaurant-group'),
-		timeDelay;
-	images.each(function(){
-		timeDelay = Math.random() * 2000 + 1000;
-		$(this).delay(timeDelay).animate({
+	$('.restaurant-group').each(function(){
+		$(this).delay(4 * delay * Math.random() + 1000).animate({
 			opacity: 1
-		}, 500);
+		}, delay);
 	});
 
 	header.animate({
 		opacity: 1
-	}, 500);
+	}, delay);
 
 	setTimeout(function(){
-		$('.intro').fadeOut(500);
-	}, 3000);
+		$('.intro').fadeOut(delay, function(){
+			$('.cover').remove();
+		});
+	}, 4 * delay + 1000);
 });
 
 $(document).ready(function(){
@@ -44,7 +45,20 @@ var more = $('.more'),
 	bot, 
 	i = 0;
 function theDeets() {
-	bot = details.hasClass('shown') ? -600 : header.height();
+	var opened = $('.opened');
+	if (opened.length > 0 && !details.hasClass('shown')) {
+		if ($(window).width() > breakPoint) {
+			openOrHideSame(opened.find('section:visible').prev());
+		} else {
+			smallScreenClose(opened.find('section:visible').prev());
+		}
+	}
+
+	if ($(window).width() > breakPoint) {
+		bot = details.hasClass('shown') ? -600 : header.height();
+	} else {
+		bot = details.hasClass('shown') ? -600 : header.height() + more.height()
+	}
 	details.toggleClass('shown').animate({
 		bottom: bot
 	});
@@ -53,41 +67,34 @@ function theDeets() {
 }
 more.click(theDeets);
 function posDeets() {
-	if (details.hasClass('shown')) {
-		details.css({
-			bottom: header.height()
-		});
+	if (details.hasClass('shown') && $(window).width() > breakPoint) {
+		bot = header.height();
+	} else if ($(window).width() <= breakPoint) {
+		bot = header.height() + more.height();
+	} else {
+		bot = -600;
 	}
+	details.css({
+		bottom: bot
+	});
 }
-$(window).on('resize', posDeets);
+$(window).resize(posDeets);
 
 // Showing descriptions for each photo
 var row = $('.row'),
-	isOneOpen = false,
 	newHeight,
 	theOpen,
 	pointer = $('<div class="pointer-container"><div class="pointer"></div></div>');
 	happening = false;
-$('section').hide();	
+// all sections are hidden behind the photos, but actually HIDE them now,
+// since we use conditional logic to see whether one is visible or not
+$('section').hide();
+
+// function to open or hide the same one when it is clicked on
 function openOrHideSame(photo) {
 	happening = true;
 	newHeight = photo.parent().height();
 	newHeight += photo.parent().hasClass('opened') ? -photo.next().outerHeight() : photo.next().outerHeight();
-
-	isOneOpen = true;
-	if (photo.next().is(':visible')) {
-		isOneOpen = false;
-	}
-
-	photo.next().show().siblings('section').hide();
-	photo.parent().toggleClass('opened').animate({
-		height: newHeight
-	}, 500, function() {
-		happening = false;
-		if (!photo.parent().hasClass('opened')) {
-			photo.parent().removeAttr('style');
-		}
-	});
 
 	// Insert the pointer
 	if (photo.next().find('.pointer-container').length == 0) {
@@ -98,110 +105,210 @@ function openOrHideSame(photo) {
 	} else {
 		setTimeout(function(){
 			pointer.remove();
-		}, 500);
+			resizeGroups();
+		}, delay);
 	}
+
+	photo.next().show().siblings('section').hide();
+	photo.parent().toggleClass('opened').animate({
+		height: newHeight
+	}, delay, function() {
+		if (!photo.parent().hasClass('opened')) {
+			photo.parent().removeAttr('style');
+		}
+		happening = false;
+	});
+
 }
+
+// If we click on a different photo in the same row
 function openSameRow(photo, theOpen) {
-	if (!happening) {
-		happening = true;
 
-		photo.parent().append('<div id="border-fake" style="height: ' + parseInt(theOpen.css('border-top')) + 'px;  top: ' + (photo.height() + 2) + 'px;"></div>').animate({
-			height: photo.parent().height() - theOpen.outerHeight() + parseInt(theOpen.css('border-top'))
-		}, 500);
+	photo.parent().append('<div id="border-fake" style="height: ' + parseInt(theOpen.css('border-top')) + 'px;  top: ' + (photo.height() + 2) + 'px;"></div>').animate({
+		height: photo.parent().height() - theOpen.outerHeight() + 40
+	}, delay);
 
-		pointer.appendTo(photo.parent()).css({
-			top: photo.outerHeight() + parseInt(theOpen.css('border-top'))
-		}).delay(500).animate({
-			left: photo.offset().left + 0.5 * photo.width() - 40
-		}, 1000, function(){
-			photo.next().prepend(pointer);
-			pointer.css({
-				top: 0
-			});
+	pointer.appendTo(photo.parent()).css({
+		top: photo.outerHeight() + parseInt(theOpen.css('border-top'))
+	}).animate({
+		left: photo.offset().left + 0.5 * photo.width() - 40
+	}, delay * 2, function(){
+		photo.next().prepend(pointer);
+		pointer.css({
+			top: 0
 		});
+	});
 
-		setTimeout(function() {
-			photo.next().show().siblings('section').hide();
-			photo.parent().animate({
-				height: photo.parent().height() + photo.next().outerHeight() - parseInt(theOpen.css('border-top'))
-			}, 500, function() {
-				happening = false;
-				$('#border-fake').remove();
-			});
-		}, 750);
-		
-	}
-}
-function openDifferentRow(photo, theOpen) {
-	if (!happening) {
-		happening = true;
-		photo.next().show();
-
-		photo.parent().addClass('opened').animate({
-			height: photo.parent().height() + photo.next().outerHeight()
-		}, 500);
-
-		theOpen.parent().removeClass('opened').animate({
-			height: theOpen.parent().height() - theOpen.outerHeight()
-		}, 500, function() {
-			theOpen.hide().parent().removeAttr('style');
+	setTimeout(function() {
+		photo.next().show().siblings('section').hide();
+		photo.parent().animate({
+			height: photo.parent().height() + photo.next().outerHeight() - 40
+		}, delay, function() {
+			$('#border-fake').remove();
 			happening = false;
 		});
-	}
+	}, delay);
 }
+
+// If we click on a photo in a different row
+function openDifferentRow(photo, theOpen) {
+	photo.next().show();
+
+	photo.parent().addClass('opened').animate({
+		height: photo.parent().height() + photo.next().outerHeight()
+	}, delay);
+
+	photo.next().prepend(pointer);
+	pointer.css({
+		left: photo.offset().left + 0.5 * (photo.width() - pointer.width())
+	});
+
+	theOpen.parent().removeClass('opened').animate({
+		height: theOpen.parent().height() - theOpen.outerHeight()
+	}, delay, function() {
+		theOpen.hide().parent().removeAttr('style');
+		happening = false;
+	});
+}
+
+// master function that determines which case we should go with (only for large screens)
 function theDescription(photo) {
-	if (!happening) {
-		// If no descriptions are open or we're closing the same one
-		if (!isOneOpen || photo.next().is(':visible')) {
-			openOrHideSame(photo);
-		// If one is already opened (and it's not the same one)
+	var opened = $('.opened');
+	happening = true;
+	// If no descriptions are open or we're closing the same one
+	if (opened.length === 0 || photo.next().is(':visible')) {
+		openOrHideSame(photo);
+	// If one is already opened (and it's not the same one)
+	} else {
+		// Same row
+		if (photo.parent().hasClass('opened')) {
+			openSameRow(photo, opened.find('section:visible'));
+		// Different row
 		} else {
-			theOpen = $('section:visible');
-			// Same row
-			if (photo.parent().hasClass('opened')) {
-				openSameRow(photo, theOpen);
-			// Different row
-			} else {
-				openDifferentRow(photo, theOpen);
-			}
+			openDifferentRow(photo, opened.find('section:visible'));
 		}
 	}
 }
-$('.restaurant-group').on('click', function(){
-	theDescription($(this));
-});
 
-function closeDescription(closer) {
-	if (!happening) {
-		happening = true;
-		isOneOpen = false;
-		closer.closest('.row').removeClass('opened').animate({
-			height: closer.closest('.row').height() - closer.closest('section').outerHeight()
-		}, 500, function(){
-			closer.closest('section').hide();
-			happening = false;
-		});
-		closer.closest(details).removeClass('shown').animate({
-			bottom: -600
-		}, 500, function(){
-			happening = false;
-		});
+// functions for small screens.
+// First, just showing one
+function smallScreenShow(photo) {
+	photo.parent().addClass('opened').siblings().removeClass('opened');
+	photo.next().prepend(pointer);
+
+	photo.animate({
+		marginBottom: photo.next().outerHeight()
+	}, delay);
+	photo.next().show().css({
+		top: (0.5 * photo.index() + 1) * photo.outerHeight() - photo.next().outerHeight()
+	}).animate({
+		top: (0.5 * photo.index() + 1) * photo.outerHeight()
+	}, delay, function(){
+		happening = false;
+	});
+}
+// closing one
+function smallScreenClose(photo) {
+	pointer.remove();
+	photo.animate({
+		marginBottom: 0
+	}, delay);
+	photo.next().animate({
+		top: (0.5 * photo.index() + 1) * photo.outerHeight() - photo.next().outerHeight()
+	}, delay, function(){
+		photo.next().hide();
+		photo.parent().removeClass('opened');
+		happening = false;
+	});
+}
+function theSmallDescription(photo) {
+	happening = true;
+	// If we're closing the same one
+	if (photo.next().is(':visible')) {
+		smallScreenClose(photo);
+	// If we're opening a different one
+	} else if ($('section:visible').length > 0) {
+		smallScreenClose($('section:visible').prev());
+		smallScreenShow(photo);
+	// If we're opening a new one
+	} else {
+		smallScreenShow(photo);
 	}
 }
-$('.closer').click(function(){
-	closeDescription($(this));
+
+$('.restaurant-group').click(function(){
+	console.log($('.opened').length);
+	if (!happening) {
+		if ($(window).width() > 600) {
+			theDescription($(this));
+		} else {
+			// small screens here
+			theSmallDescription($(this));
+		}
+		if (details.hasClass('shown')) {
+			theDeets();
+		}
+	}
 });
 
-var visibleDescription;
+// ESC to close things
+window.addEventListener('keydown', function(e) {
+	if (e.keyCode === 27) {
+		var opened = $('.opened');
+		if (opened.length > 0) {
+			if ($(window).width() > breakPoint) {
+				openOrHideSame(opened.find('section:visible').prev());
+			} else {
+				smallScreenClose(opened.find('section:visible').prev());
+			}
+		} else if (details.hasClass('shown')) {
+			theDeets();
+		}
+	}
+});
+
+// resize description on window resize, if one is visible
 function resizeDescription() {
 	var opened = $('.opened');
 	if (opened.length > 0) {
-		opened.css({
-			height: opened.find('section:visible').prev().outerHeight() + opened.find('section:visible').outerHeight()
-		});
+		var photo = opened.find('section:visible').prev();
+		if ($(window).width() > breakPoint) {
+			opened.css({
+				height: photo.outerHeight() + photo.next().outerHeight()
+			}).find(pointer).css({
+				left: photo.offset().left + 0.5 * (photo.width() - pointer.width())
+			});
+		} else {
+			photo.css({
+				marginBottom: photo.next().outerHeight()
+			}).next().css({
+				top: (0.5 * photo.index() + 1) * photo.outerHeight()
+			});
+		}
 	}
 }
 $(window).on('resize', resizeDescription);
 
-});
+// Resizing if window is taller than it is wide (and wider than the break point)
+function resizeGroups() {
+	if ($('.opened').length === 0 && $(window).width() > breakPoint) {
+		if ($(window).height() > $(window).width() + header.height()) {
+			row.height(0.333 * ($(window).height() - header.height()))
+				.find('.photo').css({
+					height: row.height() - 1,
+					left: -0.5 * (row.height() - 0.333 * row.width()),
+					width: row.height() - 1
+				});
+		} else {
+			row.height('auto').find('.photo').removeAttr('style');
+		}
+	} else if ($(window).width() > breakPoint) {
+		row.find('.photo').removeAttr('style');
+	} else if ($(window).width() <= breakPoint) {
+		row.height('auto');
+	}
+	
+}
+$(window).on('load resize', resizeGroups);
 
+});
